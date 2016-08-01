d6abe74cd79dd63d000d8b5e74c14936ab58470f
yueyingjun <1046396106@qq.com>
2016-08-01 18:25:19 +0800

    tab

--- myjquery.js
+++ myjquery.js
@@ -1,10 +1,19 @@
-function jquery(selector){
+function jquery(selector,context){
     if(typeof selector=="string"){
-        var objs=document.getElementsByTagName(selector);
-        for(var i=0;i<objs.length;i++){
-            this[i]=objs[i];
+        var context=context||[document];
+        var reg=/[\.#]?[a-z][a-z1-6_-]{0,10}\b/g;
+        var selectorArr=selector.match(reg);
+        for(var i=0;i<selectorArr.length;i++){
+            var context=this.getEle(selectorArr[i],context);
         }
-        this.length=objs.length;
+        for(var i=0;i<context.length;i++){
+            this[i]=context[i]
+        }
+        this.length=context.length;
+
+    }else  if(typeof selector=="object"){
+        this[0]=selector;
+        this.length=1;
     }
 }
 jquery.prototype={
@@ -16,7 +25,7 @@ jquery.prototype={
     click:function(callback){
         this.each(function(index,obj){
             obj.onclick=function(){
-                callback();
+                callback.call(obj);
             }
         })
 
@@ -63,11 +72,50 @@ jquery.prototype={
         })
 
         return this;
+    },
+
+    getEle:function(selector,context){
+        var arr=[];
+        if(typeof selector=="string"){
+            if(selector.charAt(0)=="."){
+                for(var i=0;i<context.length;i++){
+                    var objs=context[i].getElementsByClassName(selector.substr(1));
+                    for(var j=0;j<objs.length;j++){
+                        arr.push(objs[j]);
+                    }
+                }
+            }else if(/^[a-z][a-z1-6]{0,10}$/.test(selector)){
+                for(var i=0;i<context.length;i++){
+                    var objs=context[i].getElementsByTagName(selector);
+                    for(var j=0;j<objs.length;j++){
+                        arr.push(objs[j]);
+                    }
+                }
+            }else if(selector.charAt(0)=="#"){
+                objs=[document.getElementById(selector.substr(1))];
+            }
+        }
+        return arr;
+    },
+
+
+    eq:function(num){
+      this[0]=this[num];
+      this.length=1;
+      return this;
+    },
+    index:function(ele){
+       for(var i=0;i<this.length;i++){
+           if(this[i]==ele){
+               return i;
+           }
+       }
+
     }
 }
 
-function $(selector){
-    return new jquery(selector);
+function $(selector,context){
+    return new jquery(selector,context);
 }
 
 
