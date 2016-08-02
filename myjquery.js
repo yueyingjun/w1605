3e2e042fbef7390097e3324b9a2501eaed7c45cf
yueyingjun <1046396106@qq.com>
2016-08-02 11:37:03 +0800

    7.2

--- myjquery.js
+++ myjquery.js
@@ -1,26 +1,92 @@
-function jquery(selector,context){
-    if(typeof selector=="string"){
-        var context=context||[document];
-        var reg=/[\.#]?[a-z][a-z1-6_-]{0,10}\b/g;
-        var selectorArr=selector.match(reg);
-        for(var i=0;i<selectorArr.length;i++){
-            var context=this.getEle(selectorArr[i],context);
-        }
-        for(var i=0;i<context.length;i++){
-            this[i]=context[i]
-        }
-        this.length=context.length;
 
-    }else  if(typeof selector=="object"){
-        this[0]=selector;
-        this.length=1;
-    }
+function jquery(selector){
+    this.flag="jquery";
+  if(typeof selector=="string"){
+      if(selector.charAt(0)=="."){
+            var objs=this.getClass(selector.substr(1));
+
+            for(var i=0;i<objs.length;i++){
+                this[i]=objs[i];
+            }
+             //this.length=objs.length;
+
+           this.length=objs.length;
+
+      }else if(/^[a-z][a-z1-6]{0,10}$/.test(selector)){
+          var objs=document.getElementsByTagName(selector);
+          for(var i=0;i<objs.length;i++){
+              this[i]=objs[i];
+          }
+          //this.length=objs.length;
+
+          this.length=objs.length;
+      }else if(/^<[a-z][a-z1-6]{0,10}>$/.test(selector)){
+            var newele=document.createElement(selector.slice(1,-1));
+            this[0]=newele;
+            this.length=1;
+      }
+  }else if(typeof selector=="function"){
+      window.onload=function(){
+          selector()
+      }
+  }else if(typeof  selector=="object"&&selector.nodeType==1){
+      this[0]=selector;
+      this.length=1;
+  }else if(typeof selector=="object"&&selector.flag=="jquery"){
+      return selector;
+  }
 }
+
 jquery.prototype={
-    each:function(callback){
-        for(var i=0;i<this.length;i++)  {
-            callback(i,this[i])
+    getClass:function(classname,context){
+            var  context=context||document;
+            if(context.getElementsByClassName){
+                return context.getElementsByClassName(classname);
+            }else{
+                var arr=[];
+                var alls=context.getElementsByTagName("*");
+                for(var i=0;i<alls.length;i++){
+                    if(this.checkClass(classname,alls[i].className)){
+                        arr.push(alls[i]);
+                    }
+                }
+
+                return arr;
+            }
+    },
+
+    checkClass:function(class1,class2){
+        var arr=class2.split(" ");
+        for(var i=0;i<arr.length;i++){
+            if(arr[i]==class1){
+                return true;
+            }
         }
+        return false
+
+    },
+    html:function(val){
+        for(var i=0;i<this.length;i++){
+            this[i].innerHTML=val;
+        }
+        return this;
+    },
+    css:function(cssObj){
+        this.each(function(index,obj){
+           for(var i in cssObj){
+               if(i=="width"||i=="height"||i=="margin"){
+                   obj.style[i]=cssObj[i]+"px";
+               }else{
+                   obj.style[i]=cssObj[i]
+               }
+           }
+        })
+    },
+
+    each:function(callback){
+       for(var i=0;i<this.length;i++){
+           callback(i,this[i]);
+       }
     },
     click:function(callback){
         this.each(function(index,obj){
@@ -28,95 +94,34 @@ jquery.prototype={
                 callback.call(obj);
             }
         })
-
-        return this;
     },
-    mouseover:function(callback){
-        this.each(function(index,obj){
-            obj.onmouseover=function(){
-                callback();
-            }
-        })
-        return this;
-    },
-    html:function(val){
-        if(val){
-            this.each(function(index,obj){
-                obj.innerHTML=val;
-            })
-        }else{
-            return this[0].innerHTML;
-        }
-
-        return this;
-    },
-
-    css:function(cssObj){
-        this.each(function(index,obj){
-            for(var i in cssObj){
-                if(i=="width"||i=="height") {
-                    obj.style[i] = cssObj[i]+"px";
-                }else{
-                    obj.style[i] = cssObj[i];
-                }
-            }
-        })
-        return this;
-    },
-
-    attr:function(attrObj){
-        this.each(function(index,obj){
-            for(var i in attrObj){
-                obj.setAttribute(i,attrObj[i]);
-            }
-        })
-
-        return this;
-    },
-
-    getEle:function(selector,context){
-        var arr=[];
-        if(typeof selector=="string"){
-            if(selector.charAt(0)=="."){
-                for(var i=0;i<context.length;i++){
-                    var objs=context[i].getElementsByClassName(selector.substr(1));
-                    for(var j=0;j<objs.length;j++){
-                        arr.push(objs[j]);
-                    }
-                }
-            }else if(/^[a-z][a-z1-6]{0,10}$/.test(selector)){
-                for(var i=0;i<context.length;i++){
-                    var objs=context[i].getElementsByTagName(selector);
-                    for(var j=0;j<objs.length;j++){
-                        arr.push(objs[j]);
-                    }
-                }
-            }else if(selector.charAt(0)=="#"){
-                objs=[document.getElementById(selector.substr(1))];
-            }
-        }
-        return arr;
-    },
-
-
     eq:function(num){
-      this[0]=this[num];
       this.length=1;
+      this[0]=this[num];
       return this;
     },
+
     index:function(ele){
-       for(var i=0;i<this.length;i++){
-           if(this[i]==ele){
-               return i;
-           }
-       }
+      for(var i=0;i<this.length;i++){
+          if(ele==this[i]){
+              return i;
+          }
+      }
 
+    },
+    append:function(parent){
+      parent.appendChild(this[0]);
+        return this;
     }
+
 }
 
-function $(selector,context){
-    return new jquery(selector,context);
+function $(selector){
+    return new jquery(selector);
 }
+
+
+
 
 
 
