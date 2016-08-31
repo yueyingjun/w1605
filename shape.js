67b8a29b603f3d7e9d9be6733d79ea3a73f0cfbe
yueyingjun <1046396106@qq.com>
2016-08-31 18:28:43 +0800

    shape.js update

--- shape.js
+++ shape.js
@@ -1,60 +1,148 @@
-function shape(canvas,cobj){
-  this.canvas=canvas;
-  this.cobj=cobj;
-  this.history=[];
-  this.fillStyle="#000";
-  this.strokeStyle="#000";
-  this.lineWidth=1;
-  this.style="stroke";
-  this.type="line";
+function shape(copy,cobj,xp){
+ this.copy=copy;
+ this.cobj=cobj;
+ this.xp=xp;
+ this.canvasW=copy.offsetWidth;
+ this.canvasH=370;
+ this.fillStyle="#000";
+ this.strokeStyle="#000";
+ this.lineWidth=1;
+ this.type="line";
+ this.style="stroke";
+ this.histroy=[];
+ this.bianNum=5;
+ this.jiaoNum=5;
+ this.xpsize=10;
 }
 shape.prototype={
     init:function(){
-        this.cobj.fillStyle=this.fillStyle;
-        this.cobj.strokeStyle=this.strokeStyle;
-        this.cobj.lineWidth=this.lineWidth;
-
+      this.cobj.fillStyle=this.fillStyle;
+      this.cobj.strokeStyle=this.strokeStyle;
+      this.cobj.lineWidth=this.lineWidth;
     },
     draw:function(){
         var that=this;
-        that.canvas.onmousedown=function(e){
+        that.copy.onmousedown=function(e){
             that.init();
-            var ox= e.offsetX;
-            var oy= e.offsetY;
-            that.canvas.onmousemove=function(e){
+            var startx= e.offsetX;
+            var starty= e.offsetY;
+            that.copy.onmousemove=function(e){
+                that.cobj.clearRect(0,0,that.canvasW,that.canvasH);
+
+                if(that.histroy.length!=0){
+                    that.cobj.putImageData(that.histroy[that.histroy.length-1],0,0);
+                }
                 var movex= e.offsetX;
                 var movey= e.offsetY;
-                that.cobj.clearRect(0,0,that.canvas.width,that.canvas.height);
-                if(that.history.length!==0){
-                    that.cobj.putImageData(that.history[that.history.length-1],0,0);
-                }
-
-                that[that.type](ox,oy,movex,movey);
-
+                that[that.type](startx,starty,movex,movey);
             }
-            that.canvas.onmouseup=function(){
-                that.history.push(that.cobj.getImageData(0,0,that.canvas.width,that.canvas.height));
-                that.canvas.onmousemove=null;
-                that.canvas.onmouseup=null;
+
+            that.copy.onmouseup=function(){
+                that.histroy.push(that.cobj.getImageData(0,0,that.canvasW,that.canvasH));
+                that.copy.onmousemove=null;
+                that.copy.onmouseup=null;
             }
+
         }
     },
     line:function(x,y,x1,y1){
-      this.cobj.beginPath();
-      this.cobj.moveTo(x,y);
-      this.cobj.lineTo(x1,y1);
-      this.cobj.stroke();
+            this.cobj.beginPath();
+            this.cobj.moveTo(x,y);
+            this.cobj.lineTo(x1,y1);
+            this.cobj.stroke();
     },
     rect:function(x,y,x1,y1){
         this.cobj.beginPath();
         this.cobj.rect(x,y,x1-x,y1-y);
         this.cobj[this.style]();
-
     },
-    arc:function(x, y,x1,y1){
+    arc:function(x,y,x1,y1){
         this.cobj.beginPath();
         var r=Math.sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
-        this.cobj.arc(x,y,r,0,Math.PI*2)
+        this.cobj.arc(x,y,r,0,2*Math.PI);
         this.cobj[this.style]();
+    },
+    bian:function(x,y,x1,y1){
+        var a=360/this.bianNum*Math.PI/180;
+        var r=Math.sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
+        this.cobj.beginPath();
+        for(var i=0;i<this.bianNum;i++ ){
+            this.cobj.lineTo(x+r*Math.cos(a*i),y+r*Math.sin(a*i));
+        }
+        this.cobj.closePath();
+        this.cobj[this.style]();
+
+    },
+    jiao:function(x,y,x1,y1){
+        var a=360/(this.jiaoNum*2)*Math.PI/180;
+        var r=Math.sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
+        var r1=r/3;
+        this.cobj.beginPath();
+        for(var i=0;i<this.jiaoNum*2;i++ ){
+            if(i%2==0) {
+                this.cobj.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i))
+            }else{
+                this.cobj.lineTo(x + r1 * Math.cos(a * i), y + r1 * Math.sin(a * i))
+            }
+        }
+        this.cobj.closePath();
+        this.cobj[this.style]();
+    },
+    clear:function(){
+        var that=this;
+        that.copy.onmousemove=function(e){
+            var movex= e.offsetX;
+            var movey= e.offsetY;
+            var left=movex-that.xpsize/2;
+            var top=movey-that.xpsize/2;
+            if(left<0){
+                left=0;
+            }
+            if(left>that.canvasW-that.xpsize){
+                left=that.canvasW-that.xpsize
+            }
+            if(top<0){
+                top=0;
+            }
+            if(top>that.canvasH-that.xpsize){
+                top=that.canvasH-that.xpsize
+            }
+            that.xp.style.cssText="display:block;left:"+left+"px;top:"+top+"px;width:"+that.xpsize+"px;height:"+that.xpsize+"px";
+
+        }
+
+        that.copy.onmousedown=function(){
+
+                that.copy.onmousemove=function(e){
+                    var movex= e.offsetX;
+                    var movey= e.offsetY;
+                    var left=movex-that.xpsize/2;
+                    var top=movey-that.xpsize/2;
+                    if(left<0){
+                        left=0;
+                    }
+                    if(left>that.canvasW-that.xpsize){
+                        left=that.canvasW-that.xpsize
+                    }
+                    if(top<0){
+                        top=0;
+                    }
+                    if(top>that.canvasH-that.xpsize){
+                        top=that.canvasH-that.xpsize
+                    }
+                    that.xp.style.cssText="display:block;left:"+left+"px;top:"+top+"px;width:"+that.xpsize+"px;height:"+that.xpsize+"px";
+
+                    that.cobj.clearRect(left,top,that.xpsize,that.xpsize);
+                }
+
+            that.copy.onmouseup=function(){
+                that.histroy.push(that.cobj.getImageData(0,0,that.canvasW,that.canvasH));
+                that.copy.onmousemove=null;
+                that.copy.onmouseup=null;
+                that.clear();
+            }
+
+        }
     }
+
 }
